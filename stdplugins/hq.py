6ca4f6ffb44572762a524b9a2b7ea12fdc7a2d5d
HackerZombie <48183202+HackerZombies@users.noreply.github.com>
2019-04-14 17:01:59 +0530

    Add files via upload

---
+++ stdplugins/hq.py
@@ -0,0 +1,18 @@
+# (c) @UniBorg
+# Original written by @UniBorg edit by @INF1N17Y
+
+from telethon import events
+import asyncio
+from collections import deque
+
+
+@borg.on(events.NewMessage(pattern=r".hq", outgoing=True))
+async def _(event):
+	if event.fwd_from:
+		return
+	deq = deque(list("🤣-<—< 🤣-<—< 🤣-<—< 🤣-<—<"))
+	for _ in range(100000000):
+		await asyncio.sleep(0.1)
+		await event.edit("".join(deq))
+		deq.rotate(1)
+    
